9543eced037ba2cf6a26cb6f9a5bb851a778d472
Vida Maleki <vida.basmani@gmail.com>
2022-07-25 15:32:00 -0700

    Home page Navbar working, only Video left for home page

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Calculator from "./pages/Calculator/Calculator";
 import CurrencyConverter from "./pages/CurrencyConverter/CurrencyConverter";
 import LoanCalculator from "./pages/LoanCalculator/LoanCalculator";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact component={<Home />} />
-          <Route path="/calculator" element={Calculator} />
-          <Route path="/currencyconverter" element={CurrencyConverter} />
-          <Route path="/loancalculator" element={LoanCalculator} />
-        </Routes>
-      </Router>
-    </>
-  );
+    return (
+        <>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/calculator" element={<Calculator/>} />
+              <Route path="/currencyconverter" element={<CurrencyConverter/>} />
+              <Route path="/loancalculator" element={<LoanCalculator/>} />
+            </Routes>
+          </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- calculator/src/components/SidebarData.js
+++ calculator/src/components/SidebarData.js
@@ -4,28 +4,28 @@ import * as AiIcons from "react-icons/ai";
 import * as BsIcons from "react-icons/bs";
 
 export const SidebarData = [
-  {
+    {
     title: "Home",
     path: "/",
     icon: <AiIcons.AiFillHome />,
     cName: "nav-text",
-  },
-  {
+    },
+    {
     title: "Calculator",
     path: "/calculator",
     icon: <FaIcons.FaCalculator />,
     cName: "nav-text",
-  },
-  {
+    },
+    {
     title: "Currency Converter",
     path: "/currencyconverter",
     icon: <BsIcons.BsCurrencyExchange />,
     cName: "nav-text",
-  },
-  {
+    },
+    {
     title: "Loan Calculator",
     path: "/loancalculator",
     icon: <FaIcons.FaClipboardList />,
     cName: "nav-text",
-  },
+    },
 ];

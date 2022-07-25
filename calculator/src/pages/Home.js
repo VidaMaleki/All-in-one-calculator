648019a7a8a43d298f90d08862d8d45b341135e3
maria <camilatagleo@gmail.com>
2022-07-25 16:30:58 -0700

    Navbar ready, Background video in process

--- calculator/src/pages/Home.js
+++ calculator/src/pages/Home.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import backgroundVideo from "../video/backgroundVideo.mp4";
+import Navbar from "../components/Navbar";
 
 function Home() {
-    return (
-        <div className='home'>
-        <h1>Home</h1>
+  return (
+    <div className="main">
+      <div className="home">
+        <div className="overlay"></div>
+        <video src={backgroundVideo} autoPlay loop muted id="video" />
+        <div className="content">
+          <h1>Home</h1>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;

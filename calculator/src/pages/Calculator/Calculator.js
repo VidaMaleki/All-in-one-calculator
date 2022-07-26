05f41b2302f89ef4edb67d8773006b92e83af11f
maria <camilatagleo@gmail.com>
2022-07-26 11:41:59 -0700

    started calculator

--- calculator/src/pages/Calculator/Calculator.js
+++ calculator/src/pages/Calculator/Calculator.js
@@ -1,9 +1,24 @@
 import React from "react";
+import CalculatorBox from "./CalculatorBox";
+import Button from "./Button";
+import { Textfit } from "react-textfit";
 
 function Calculator() {
   return (
     <div className="calculator">
       <h1>Calculator</h1>
+      <CalculatorBox>
+        <Textfit value="0" className="screen" mode="single" max={70}>
+          {/* {value='0'} */}
+        </Textfit>
+        <Button
+          className=""
+          value="0"
+          onClick={() => {
+            console.log("Button clicked");
+          }}
+        />
+      </CalculatorBox>
     </div>
   );
 }
---
+++ calculator/src/pages/Calculator/CalculatorBox.js
@@ -0,0 +1,12 @@
+
+import './CalculatorBox.css'
+
+const CalculatorBox = (props) => {
+  return (
+    <div className="wrapper">
+      <div className="buttonBox"></div>
+    </div>
+  );
+};
+
+export default CalculatorBox;

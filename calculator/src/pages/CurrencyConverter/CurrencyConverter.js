96adecd27c7d0885a35f33c95698e92837e54fa4
maria <camilatagleo@gmail.com>
2022-08-01 11:58:08 -0700

    currency converter in progress

--- calculator/src/pages/CurrencyConverter/CurrencyConverter.js
+++ calculator/src/pages/CurrencyConverter/CurrencyConverter.js
@@ -1,9 +1,30 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 function CurrencyConverter() {
+  const fromCurr = "IDR";
+  const toCurr = "JPY";
+  const accesKey = "API_KEY_GOES_HERE";
+  const [exRate, setExRate] = useState("");
+  const URL = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${fromCurr}&to_currency=${toCurr}&apikey=${accesKey}`;
+  const exchInfo = () => {
+    axios
+      .get(URL)
+      .then((response) => {
+        const newResponse = response.data;
+        setExRate(newResponse);
+        console.log(newResponse);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <div className="currencyconverter">
-      <h1>Currency Converter</h1>
+    <div className="App">
+      <p>`Hola esta es la info`</p>
+      <button onClick={exchInfo}>get exchange rate</button>
+      <p>`esto es {exRate.data}`</p>
     </div>
   );
 }

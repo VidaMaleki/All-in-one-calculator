593230bb6d6c623b2b4f5d61b756e8af5fb13cfa
maria <camilatagleo@gmail.com>
2022-08-02 16:44:16 -0700

    Currency Converter

--- calculator/src/pages/CurrencyConverter/CurrencyConverter.js
+++ calculator/src/pages/CurrencyConverter/CurrencyConverter.js
@@ -1,21 +1,30 @@
 import React from "react";
 import axios from "axios";
 import { useState } from "react";
-import ExchangeForm from "./CurrencyForm";
+import Dropdown from "react-dropdown";
+import currenciesDictionary from "./DropDown.js";
 
 function CurrencyConverter() {
-  const fromCurr = "IDR";
-  const toCurr = "JPY";
-  const accesKey = "API_KEY_GOES_HERE";
+  function getObjKey(obj, value) {
+    return Object.keys(obj).find((key) => obj[key] === value);
+  }
+
+  const [fromCurr, setFromCurr] = useState("USD");
+  const [toCurr, setToCurr] = useState("JPY");
   const [exRate, setExRate] = useState("");
-  const URL = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${fromCurr}&to_currency=${toCurr}&apikey=${accesKey}`;
+  const [info, setInfo] = useState("Previous");
+  const URL = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${fromCurr}&to_currency=${toCurr}&apikey=S6OUYUH5FUD5HOY6`;
+
   const exchInfo = () => {
     axios
       .get(URL)
       .then((response) => {
         const newResponse = response.data;
         setExRate(newResponse);
+        setInfo(newResponse);
         console.log(newResponse);
+        console.log("hola");
+        console.log(info);
       })
       .catch((err) => {
         console.log(err);
@@ -23,7 +32,29 @@ function CurrencyConverter() {
   };
   return (
     <div className="App">
-      <ExchangeForm />
+      <form>
+        <div className="CurrAmount">
+          <label>Currency Amount</label>
+          <input type="text" />
+          <Dropdown
+            options={Object.values(currenciesDictionary)}
+            onChange={(e) => {
+              setFromCurr(String(getObjKey(currenciesDictionary, e.value)));
+            }}
+          />
+        </div>
+
+        <div className="CurrAmount">
+          <label>Currency Amount</label>
+          <input type="text" />
+          <Dropdown
+            options={Object.values(currenciesDictionary)}
+            onChange={(e) => {
+              setToCurr(String(getObjKey(currenciesDictionary, e.value)));
+            }}
+          />
+        </div>
+      </form>
       <button onClick={exchInfo}>get exchange rate</button>
     </div>
   );
